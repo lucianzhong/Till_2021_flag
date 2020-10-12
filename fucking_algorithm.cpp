f3bbf0daa1e9787a87c49a784b6d98a7b3d5c51e
lucianzhong <lucianzhong@gmail.com>
2020-10-12 22:53:07 +0800

    bfs dfs

---
+++ fucking_algorithm.cpp
@@ -0,0 +1,9 @@
+
+
+https://labuladong.gitbook.io/algo/di-ling-zhang-bi-du-xi-lie/xue-xi-shu-ju-jie-gou-he-suan-fa-de-gao-xiao-fang-fa
+
+
+1. 
+BFS  DFS
+https://labuladong.gitbook.io/algo/di-ling-zhang-bi-du-xi-lie/bfs-kuang-jia
+https://developer.aliyun.com/article/756316
---
+++ gophae博客.cpp
@@ -0,0 +1,64 @@
+
+// 数学基础
+
+1. 离散点求导的数学理论与方法
+https://blog.csdn.net/gophae/article/details/102518309
+
+
+2. 非线性回归的数学理论与方法(非线性最小二乘法)
+https://blog.csdn.net/gophae/article/details/102518428
+
+
+3. 三阶样条曲线
+ https://blog.csdn.net/gophae/article/details/102808083
+<< numerical methods for engineers >>
+
+// 笔记
+https://kluge.in-chemnitz.de/opensource/spline/
+
+
+4.  BFS， DFS， A*
+// DFS总是向最深的方向搜索
+// BFS使用queue,先进的先出
+// DFS用到了栈结构，先进后出
+// 这里可以看到，广度优先搜索会 比深度优先搜索范围更大，虽然可以绝对保证最优性结果，但是效率远不及深度优先，而深度优先的问题则在于无法保证最优
+
+
+5. 自动驾驶中的滞后碰撞检测(lazy-collision-checking)
+https://blog.csdn.net/gophae/article/details/103785393
+
+<< Fast Collision Checking for Intelligent Vehicle Motion Planning >>
+<< Dynamic Motion Planning Framework for Autonomous Driving in Urban Environments >>
+
+
+6. Matlab解析LQR与MPC的关系
+https://blog.csdn.net/gophae/article/details/104546805
+
+
+
+7. 离散点曲率拟合
+https://blog.csdn.net/gophae/article/details/104296957
+
+
+
+8. LQR轨迹跟踪算法Python/Matlab算法实现_LQRmatrix推导
+https://blog.csdn.net/gophae/article/details/104538585
+
+
+9. Matlab解析LQR与MPC的关系
+https://blog.csdn.net/gophae/article/details/104546805
+
+
+// 笔记
+10. PID参数整定法
+https://blog.csdn.net/gophae/article/details/104554398
+
+
+
+11.
+对于原始轨迹的噪声过滤方法和曲率计算方法
+对于地图采样获得的轨迹，通过cross entropy的方式，进行噪声过滤。曲率计算采用三点式计算圆半径获得
+https://blog.csdn.net/gophae/article/details/104661890
+
+
+12.
